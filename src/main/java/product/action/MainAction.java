39bccf74ce73b1fb76248996f0ecc9168f9e29db
hyunjeong <tgc99035@naver.com>
2023-04-28 21:27:40 +0900

    짜잘

--- src/main/java/product/action/MainAction.java
+++ src/main/java/product/action/MainAction.java
@@ -25,7 +25,7 @@ public class MainAction implements Action {
 		
 		
 		request.setAttribute("productList", productList);
-	    request.setAttribute("pagefile", "/product/productBest.jsp");
+	    request.setAttribute("pagefile", "/main.jsp");
 		forward = new ActionForward("/index.jsp",false);//보여주는경로
 		forward.setPath("/index.jsp");
 		return forward;
