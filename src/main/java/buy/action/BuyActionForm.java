13a6a8cf85dfd2ef3ea29eda50f79e39a5ba7c4f
hyunjeong <tgc99035@naver.com>
2023-05-08 11:11:09 +0900

    buyaction

--- src/main/java/buy/action/BuyActionForm.java
+++ src/main/java/buy/action/BuyActionForm.java
@@ -14,44 +14,45 @@ import vo.Delivery;
 
 public class BuyActionForm implements Action {
 
-	@Override//액션에서는
-	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		ActionForward forward = null;//널처리해주고
-		String id = request.getParameter("id");//파라미터처리해주고 //아이디
-		String[] p_num = request.getParameterValues("p_num");//상품번호
-		System.out.println(Arrays.toString(p_num));
-		String[] buy_qty = request.getParameterValues("buy_qty");//구매수량
-		String[] p_price = request.getParameterValues("p_price");//상품금액
-		String[] cart_num= null;
-		if(request.getParameterValues("cart_num") != null) {
-			cart_num = request.getParameterValues("cart_num");
-			request.setAttribute("cart_num", cart_num);//cart번호
-		}
-		
-		int lastTotalMoney=0;
-		
-		ArrayList<Buy> buyList = new ArrayList<Buy>();
-		for(int i=0; i<p_num.length;i++) {
-			Buy buy = new Buy();
-			buy.setId(id); //아이디 set
-			buy.setP_num(Integer.parseInt(p_num[i])); //상품번호 set
-			buy.setBuy_qty(Integer.parseInt(buy_qty[i])); //수량 set
-			buy.setBuy_totalmoney(Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i])); //총금액set
-			buyList.add(buy);
-			lastTotalMoney+=Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i]);
-			
-		}
-		
-		
-		DeliveryListService deliveryListService = new DeliveryListService();
-		ArrayList<Delivery> deliveryList = deliveryListService.getDeliveryList(id);
-		request.setAttribute("deliveryList", deliveryList); //배송지리스트
-		request.setAttribute("lastTotalMoney", lastTotalMoney); //찐 전체금액
-		request.setAttribute("buyList", buyList); //구매목록들
-		request.setAttribute("pagefile","/buy/buyForm.jsp");
-		forward = new ActionForward("index.jsp",false);
-		//
-		return forward;
-	}
+   @Override//액션에서는
+   public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+      ActionForward forward = null;//널처리해주고
+      String id = request.getParameter("id");//파라미터처리해주고 //아이디
+      String[] p_num = request.getParameterValues("p_num");//상품번호
+      System.out.println(Arrays.toString(p_num));
+      String[] buy_qty = request.getParameterValues("buy_qty");//구매수량
+      String[] p_price = request.getParameterValues("p_price");//상품금액
+      
+      String[] cartList=null;//cart없애보자.
+      if(request.getParameterValues("cart_num") != null) {
+         cartList = request.getParameterValues("cart_num");
+      }
+      
+      int lastTotalMoney=0;
+      
+      ArrayList<Buy> buyList = new ArrayList<Buy>();
+      for(int i=0; i<p_num.length;i++) {
+         Buy buy = new Buy();
+         buy.setId(id); //아이디 set
+         buy.setP_num(Integer.parseInt(p_num[i])); //상품번호 set
+         buy.setBuy_qty(Integer.parseInt(buy_qty[i])); //수량 set
+         buy.setBuy_totalmoney(Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i])); //총금액set
+         buyList.add(buy);
+         lastTotalMoney+=Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i]);
+         
+      }
+      
+      
+      DeliveryListService deliveryListService = new DeliveryListService();
+      ArrayList<Delivery> deliveryList = deliveryListService.getDeliveryList(id);
+      request.setAttribute("cartList", cartList);//cart번호
+      request.setAttribute("deliveryList", deliveryList); //배송지리스트
+      request.setAttribute("lastTotalMoney", lastTotalMoney); //찐 전체금액
+      request.setAttribute("buyList", buyList); //구매목록들
+      request.setAttribute("pagefile","/buy/buyForm.jsp");
+      forward = new ActionForward("index.jsp",false);
+      //
+      return forward;
+   }
 
 }
