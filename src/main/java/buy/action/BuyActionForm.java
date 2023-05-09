6705371859bbabea3ea466f6edb95d82c2f7f034
hyunjeong <tgc99035@naver.com>
2023-05-09 12:42:31 +0900

    장바구니없을때 돌아가기 가능함

--- src/main/java/buy/action/BuyActionForm.java
+++ src/main/java/buy/action/BuyActionForm.java
@@ -11,6 +11,7 @@ import action.Action;
 import delivery.svc.DeliveryListService;
 import vo.ActionForward;
 import vo.Buy;
+import vo.Cart;
 import vo.Delivery;
 
 public class BuyActionForm implements Action {
@@ -25,12 +26,56 @@ public class BuyActionForm implements Action {
       
       
       System.out.println("이거 뭐야"+request.getParameterValues("remove"));
-      if(request.getParameterValues("remove") == null && request.getParameterValues("p_num") != null){
-  	      id = request.getParameter("id");//파라미터처리해주고 //아이디
-          p_num = request.getParameterValues("p_num");//상품번호
-          System.out.println(Arrays.toString(p_num));
-          buy_qty = request.getParameterValues("buy_qty");//구매수량
-          p_price = request.getParameterValues("p_price");//상품금액
+      //remove값이 null일때,
+      if(request.getParameterValues("remove") == null){//장바구니널일때,
+    	  p_num = request.getParameterValues("p_num");
+    	  if(p_num.length == 1) {//p_num이 한개일때
+    		  id = request.getParameter("id");//파라미터처리해주고 //아이디
+              p_num = request.getParameterValues("p_num");//상품번호
+              buy_qty = request.getParameterValues("buy_qty");//구매수량
+              p_price = request.getParameterValues("p_price");//상품금액
+              
+              int lastTotalMoney=0;
+              
+              ArrayList<Buy> buyList = new ArrayList<Buy>();
+              for(int i=0; i<p_num.length;i++) {
+                 Buy buy = new Buy();
+                 buy.setId(id); //아이디 set
+                 buy.setP_num(Integer.parseInt(p_num[i])); //상품번호 set
+                 buy.setBuy_qty(Integer.parseInt(buy_qty[i])); //수량 set
+                 buy.setBuy_totalmoney(Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i])); //총금액set
+                 buyList.add(buy);
+                 lastTotalMoney+=Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i]);
+                 
+              }
+              
+              
+              DeliveryListService deliveryListService = new DeliveryListService();
+              ArrayList<Delivery> deliveryList = deliveryListService.getDeliveryList(id);
+              request.setAttribute("deliveryList", deliveryList); //배송지리스트
+              request.setAttribute("lastTotalMoney", lastTotalMoney); //찐 전체금액
+              request.setAttribute("buyList", buyList); //구매목록들
+              request.setAttribute("pagefile","/buy/buyForm.jsp");
+              forward = new ActionForward("index.jsp",false);
+    	  }else {
+    		  response.setContentType("text/html;charset=UTF-8"); 
+    		  PrintWriter out = response.getWriter(); out.println("<script>");
+    		  out.println("alert('구매하실 상품을 선택해주세요')"); 
+    		  out.println("history.back()");
+    		  out.println("</script>");
+    	  }
+    	  
+      }else {//장바구니가 널아닐때,
+    	  String[] cartList=request.getParameterValues("remove");//장바구니없애기용 cart번호
+    	  System.out.println("카트넘버"+Arrays.toString(cartList));
+    	  //카트넘버랑 아이디 받아와서,카트에 있는 상품번호구매수량, 상품금액 가져오기
+          id = request.getParameter("id");//파라미터처리해주고 //아이디
+          ArrayList<Cart> cartSet = new ArrayList<Cart>();
+          
+          
+//          p_num = request.getParameterValues("p_num");//상품번호
+//          buy_qty = request.getParameterValues("buy_qty");//구매수량
+//          p_price = request.getParameterValues("p_price");//상품금액
           
           int lastTotalMoney=0;
           
@@ -49,53 +94,14 @@ public class BuyActionForm implements Action {
           
           DeliveryListService deliveryListService = new DeliveryListService();
           ArrayList<Delivery> deliveryList = deliveryListService.getDeliveryList(id);
+          request.setAttribute("cartList", cartList);//cart번호
           request.setAttribute("deliveryList", deliveryList); //배송지리스트
           request.setAttribute("lastTotalMoney", lastTotalMoney); //찐 전체금액
           request.setAttribute("buyList", buyList); //구매목록들
           request.setAttribute("pagefile","/buy/buyForm.jsp");
           forward = new ActionForward("index.jsp",false);
+    	 
       }
-      else if(request.getParameterValues("remove") == null) {
- 	         response.setContentType("text/html;charset=UTF-8"); 
-			 PrintWriter out = response.getWriter(); out.println("<script>");
-			 out.println("alert('구매하실 상품을 선택해주세요')"); 
-			 out.println("history.back()");
-			 out.println("</script>");
-      }
-      else if(request.getParameterValues("remove") != null) {
-    	 String[] cartList=request.getParameterValues("remove");//cart없애보자.
-         id = request.getParameter("id");//파라미터처리해주고 //아이디
-         p_num = request.getParameterValues("p_num");//상품번호
-         buy_qty = request.getParameterValues("buy_qty");//구매수량
-         p_price = request.getParameterValues("p_price");//상품금액
-         
-         int lastTotalMoney=0;
-         
-         ArrayList<Buy> buyList = new ArrayList<Buy>();
-         for(int i=0; i<p_num.length;i++) {
-            Buy buy = new Buy();
-            buy.setId(id); //아이디 set
-            buy.setP_num(Integer.parseInt(p_num[i])); //상품번호 set
-            buy.setBuy_qty(Integer.parseInt(buy_qty[i])); //수량 set
-            buy.setBuy_totalmoney(Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i])); //총금액set
-            buyList.add(buy);
-            lastTotalMoney+=Integer.parseInt(buy_qty[i])*Integer.parseInt(p_price[i]);
-            
-         }
-         
-         
-         DeliveryListService deliveryListService = new DeliveryListService();
-         ArrayList<Delivery> deliveryList = deliveryListService.getDeliveryList(id);
-         request.setAttribute("cartList", cartList);//cart번호
-         request.setAttribute("deliveryList", deliveryList); //배송지리스트
-         request.setAttribute("lastTotalMoney", lastTotalMoney); //찐 전체금액
-         request.setAttribute("buyList", buyList); //구매목록들
-         request.setAttribute("pagefile","/buy/buyForm.jsp");
-         forward = new ActionForward("index.jsp",false);
-      }
-      
-      
-      //
       return forward;
    }
 
