85825ade0b5d26cc3f81b80eb3f9405db78eb716
kimgeonhyeong <kkh6246@gmail.com>
2023-05-09 12:42:09 +0900

    장바구니 프로덕트 건드렸어요

--- src/main/java/cart/action/CartAddAction.java
+++ src/main/java/cart/action/CartAddAction.java
@@ -1,7 +1,6 @@
 package cart.action;
 
 import java.io.PrintWriter; 
-import java.sql.Connection;
 
 import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse;
@@ -9,11 +8,8 @@ import javax.servlet.http.HttpSession;
 
 import action.Action;
 import cart.svc.CartAddService;
-import cart.svc.CartQtyUpService;
-import dao.CartDAO;
 import vo.ActionForward;
 import vo.Cart;
-import static db.JdbcUtil.*;
 
 public class CartAddAction implements Action {
 
@@ -26,27 +22,32 @@ public class CartAddAction implements Action {
 		String numParam = request.getParameter("p_num");
 		int num = numParam == null ? 0 : Integer.parseInt(numParam);
 
-
 		Cart cart = new Cart();
 		cart.setP_num(num);
 		cart.setId(id);
 
 		CartAddService cartAddService = new CartAddService();
-
-		
 		boolean isAddSuccess = cartAddService.addCart(cart);
+
 		if (isAddSuccess == false) {
-			 System.out.println(isAddSuccess);
-			 response.setContentType("text/html;charset=UTF-8"); 
-			 PrintWriter out = response.getWriter(); out.println("<script>");
-			 out.println("alert('이미 장바구니에 담긴 상품입니다!')"); 
-			 out.println("history.back()");
-			 out.println("</script>");
-		}else {
-			forward = new ActionForward();
-			forward.setRedirect(true);
-			forward.setPath("cartList.ct?p_num=" + num); 
+			response.setContentType("text/html;charset=UTF-8");
+			PrintWriter out = response.getWriter();
+			out.println("<script>");
+			out.println("alert('이미 장바구니에 담긴 상품입니다!')");
+			out.println("history.back()");
+			out.println("</script>");
+			out.flush(); // 버퍼 비우기
+			return forward;
 		}
+
+		// 장바구니 추가 후 팝업창 띄우기
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<script>");
+		out.println("if(confirm('장바구니에 상품을 담았습니다. 장바구니로 이동하시겠습니까?')){");
+		out.println("location.href='cartList.ct';}");
+		out.println("</script>");
+		out.flush(); // 버퍼 비우기
 
 		return forward;
 	}
--- src/main/webapp/product/productDetailView.jsp
+++ src/main/webapp/product/productDetailView.jsp
@@ -35,7 +35,7 @@
                     <button type="submit" class="btn btn-primary my-3">구매하기</button>
 
                     <button type="button" class="btn btn-secondary my-3" 
-                    onclick="location.href='cartAddAction.ct?p_num=${product.p_num}'">
+                   onclick="confirmCartMove()">
                     장바구니</button>
 
                     </c:if>
@@ -51,6 +51,13 @@
             </div>
         </div>
     </div>
-    
+    <script>
+    function confirmCartMove() {
+    	  var result = confirm("장바구니에 상품을 등록하시겠습니까?");
+    	  if (result) {
+    	    location.href = 'cartAddAction.ct?p_num=${product.p_num}';
+    	  }
+    	}
+    </script>
 </body>
 </html>
